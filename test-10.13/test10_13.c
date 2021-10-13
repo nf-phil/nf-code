2604db7649cbb4a607f762277df7227eb0f9cfff
nf-phil <1466258586@qq.com>
2021-10-13 13:49:57 +0800

    学习10.13

---
+++ test-10.13/test10_13.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//≤‚ ‘Ã‚
+
+//#include<stdio.h>
+//int cnt = 0;
+//int fib(int n)
+//{
+//	cnt++;
+//	if (n == 0)
+//		return 1;
+//	else if (n == 1)
+//		return 2;
+//	else
+//		return fib(n - 1) + fib(n - 2)
+//		;
+//}
+//void main()
+//{
+//	fib(8);
+//	printf("%d", cnt);//67
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int x = 1;
+//	do 
+//	{
+//		printf("%2d\n", x++);
+//	} while (x--);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	int j ;
+//	j = i++;
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 10;
+//	int j = 20;
+//	int k = 3;
+//	k *= i + j;
+//	printf("%d\n", k);//90
+//	return 0;
+//
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int a = 1;
+//void test()
+//{
+//	int a = 2;
+//	a += 1;
+//}
+//int main()
+//{
+//	test();
+//	printf("%d\n", a);//1
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
